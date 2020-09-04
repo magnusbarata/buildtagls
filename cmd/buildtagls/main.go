44f16e41a5fc095abcded62d49951ff7d71e048d
magnusbarata <barata.magnuz@gmail.com>
2020-09-04 19:22:44 +0900

    fix import path

--- cmd/buildtagls/main.go
+++ cmd/buildtagls/main.go
@@ -1,9 +1,9 @@
-package main
+package main 
+
 
 import (
         "github.com/magnusbarata/buildtagls"
         "golang.org/x/tools/go/analysis/unitchecker"
 )
-
 
 func main() { unitchecker.Main(buildtagls.Analyzer) }
